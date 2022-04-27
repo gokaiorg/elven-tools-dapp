986090deed8a3e7a2eb997f2ef633aac758ce0ec
Jeremy <jeremydouchamps@gmail.com>
2022-04-27 20:48:21 +0700

    Test sitemaps

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,5 +2,10 @@
 
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://gokai.org',
-    generateRobotsTxt: true,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://gokai.org/sitemap-1.xml',
+    ],
+  },
 }
