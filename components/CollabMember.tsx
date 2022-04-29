030d08195684f7813ae61dce0406e2e2d3b43f51
Jeremy <jeremydouchamps@gmail.com>
2022-04-29 09:48:41 +0700

    Debug links

--- components/CollabMember.tsx
+++ components/CollabMember.tsx
@@ -5,27 +5,31 @@ import { FC } from 'react';
 interface CollabMemberProps {
   name: string;
   imageUrl: string;
+  Links: string;
   socialMediaLinks?: string[];
 }
 
 export const CollabMember: FC<CollabMemberProps> = ({
   name,
   imageUrl,
+  Links,
   socialMediaLinks,
 }) => {
   return (
     <Box color="elvenTools.color3.base">
       {socialMediaLinks && (
-        <Box
-          as="a"
-          height="120px"
-          fontSize="xl"
-          display="flex"
-          href={socialMediaLinks}
-          justifyContent="center"
-        >
+        <Box>
           {socialMediaLinks.map((link, index) => (
-            <Image src={imageUrl} alt={name} width="100%" height="100%" objectFit="contain" />
+            <Box
+              as='a'
+              height="120px"
+              fontSize="xl"
+              display="flex"
+              href={Links}
+              justifyContent="center"
+            >
+              <Image src={imageUrl} alt={name} width="100%" height="100%" objectFit="contain" />
+            </Box>
           ))}
         </Box>
       )}
