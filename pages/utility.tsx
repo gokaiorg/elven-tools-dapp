f4a4e8c2189fa143bec0c917182f9911969bf04d
Jeremy <jeremydouchamps@gmail.com>
2022-05-27 15:21:00 +0700

    Add utility

---
+++ pages/utility.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from 'next';
+import { Box, Text } from '@chakra-ui/react';
+import { MainLayout } from '../components/MainLayout';
+import { HeaderMenu } from '../components/HeaderMenu';
+import { HeaderMenuButtons } from '../components/HeaderMenuButtons';
+import { Utility } from '../components/Utility';
+
+const Mint: NextPage = () => {
+  return (
+    <MainLayout>
+      <HeaderMenu>
+        <HeaderMenuButtons enabled={['auth', 'rarity', 'maiar-ghosts', 'utility']} />
+      </HeaderMenu>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        position="relative"
+      >
+        <Text
+          as="h1"
+          fontSize={{ base: '4xl', md: '3xl', lg: '5xl' }}
+          maxWidth={{ base: '100%', md: '100%', lg: '60%' }}
+          textAlign={{ base: 'center', md: 'center' }}
+          fontWeight="black"
+          lineHeight="shorter"
+          mx="auto"
+          pt={{ base: '16', lg: '20' }}
+          mb={5}
+          zIndex="10"
+          color="elvenTools.color3.base"
+        >
+          Utility
+        </Text>
+        <Utility/>
+      </Box>
+    </MainLayout>
+  );
+};
+
+export default Mint;
