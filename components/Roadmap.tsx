38e249f1dba4ca0a522b3a2803d9981694819fe1
Jeremy <jeremydouchamps@gmail.com>
2022-12-17 18:13:14 +0700

    Add GreenGarden Link

--- components/Roadmap.tsx
+++ components/Roadmap.tsx
@@ -12,6 +12,7 @@ export const Roadmap = () => {
         width="100%"
         position="relative"
         zIndex="1"
+        mb={20}
         pt={{ base: '5', md: '32', lg: '0' }}
       >
         <Text
@@ -20,10 +21,56 @@ export const Roadmap = () => {
           fontWeight="medium"
           textAlign="center"
           zIndex="10"
+          top="-3rem"
+          position="relative"
           color="black"
         >
           Gokai Labs is a contributor and community member of the GhostVerse DAO
           on MultiversX.
+        </Text>
+        <Box display="flex" position="relative" zIndex="10" m="0 auto">
+          <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+            <Text
+              as="a"
+              fontSize="xl"
+              display="flex"
+              p="1rem 2rem"
+              my={5}
+              color="elvenTools.color2.base"
+              background="elvenTools.color2.lighter"
+              _hover={{
+                bg: 'elvenTools.color2.darker',
+                color: 'elvenTools.color2.lighter',
+              }}
+              borderRadius="full"
+              href="https://ghostverse.org/"
+            >
+              Visit
+            </Text>
+          </motion.div>
+        </Box>
+      </Box>
+
+      <HomeSectionTitle title="GreenGarden Cannabis Shop" />
+      <Box
+        display="flex"
+        flexDirection="column"
+        width="100%"
+        position="relative"
+        zIndex="1"
+        pt={{ base: '5', md: '32', lg: '0' }}
+      >
+        <Text
+          as="p"
+          fontSize="2xl"
+          fontWeight="medium"
+          textAlign="center"
+          zIndex="10"
+          position="relative"
+          top="-3rem"
+          color="black"
+        >
+          Gokai Labs is building a tokenized cannabis shop in Thailand.
         </Text>
         <Box display="flex" position="relative" zIndex="10" m="0 auto">
           <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
@@ -40,9 +87,9 @@ export const Roadmap = () => {
                 color: 'elvenTools.color2.lighter',
               }}
               borderRadius="full"
-              href="https://ghostverse.org/"
+              href="https://green.gd/"
             >
-              Visit
+              LFGREEN
             </Text>
           </motion.div>
         </Box>
